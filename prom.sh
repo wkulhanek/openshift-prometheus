4616f00eadfbc3460ed67e1452cd54504c1edd73
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-07-05 14:28:47 -0400

    Updated project name

--- prom.sh
+++ prom.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-oc new-project monitoring
+oc new-project prometheus --display-name="Monitoring - Prometheus"
 oc create -f prometheus_3.5.yaml
-oc new-app prometheus -p NAMESPACE=monitoring
+oc new-app prometheus -p NAMESPACE=prometheus

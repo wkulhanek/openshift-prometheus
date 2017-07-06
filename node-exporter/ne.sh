0bed9b76a0655313b6303542007daf42002bfd1f
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-07-06 15:46:50 -0400

    Added comments

--- node-exporter/ne.sh
+++ node-exporter/ne.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 oc new-project exporter --display-name="Monitoring - Node Exporter"
-oc create -f node-exporter.yaml
+oc create -f node-exporter-template.yaml
 oc new-app node-exporter -p NAMESPACE=exporter
-oc adm policy add-scc-to-user privileged system:serviceaccount:exporter:default
+oc adm policy add-scc-to-user hostaccess system:serviceaccount:exporter:default
 
 # Next line is necessary to place pods not just on worker nodes but also
 # on Infranodes and Masters
+# If there are still pods with a "MatchNodeSelector" error simply delete them
+# and they will be recreated successfully
 oc annotate namespace exporter openshift.io/node-selector=monitoring=true
+
+# Now label all nodes with the right label for the DaemonSet to place
+# a node-exporter pod on each node
 oc label node node1.mon.internal monitoring-true
 oc label node node2.mon.internal monitoring-true
 oc label node infranode1.mon.internal monitoring-true

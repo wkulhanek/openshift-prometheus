f4ddc668cec5b84b2747cb77d6782e7ae9d6a8a7
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-07-12 15:49:12 -0400

    Updated docker info collection script to use lvs instead of 'docker info'

--- node-exporter/dockerinfo/dockerinfo.sh
+++ node-exporter/dockerinfo/dockerinfo.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # Collect Docker Volume Group Space Information
-docker info 2>/dev/null | grep "Data Space Total"     | awk '{print "node_docker_volume_size_bytes      "int($4*1024*1024)}'  >/tmp/docker_size.prom
-docker info 2>/dev/null | grep "Data Space Used"      | awk '{print "node_docker_volume_used_bytes      "int($4*1024*1024)}' >>/tmp/docker_size.prom
-docker info 2>/dev/null | grep "Data Space Available" | awk '{print "node_docker_volume_available_bytes "int($4*1024*1024)}' >>/tmp/docker_size.prom
-mv /tmp/docker_size.prom /var/lib/node_exporter/textfile_collector
+lvs docker-vg --units b |grep docker-pool|awk -c '{print "node_docker_volume_size_bytes $4"}'         >/tmp/docker_info.prom
+lvs docker-vg --units b |grep docker-pool|awk -c '{print "node_docker_volume_data_percent_full $5"}' >>/tmp/docker_info.prom
+lvs docker-vg --units b |grep docker-pool|awk -c '{print "node_docker_volume_meta_percent_full $6"}' >>/tmp/docker_info.prom
+echo "{node_docker_running_containers " $(docker ps -q |wc -l) "}" >>/tmp/docker_info.prom
+echo "node_docker_last_successful_update `date +%s`" >>/tmp/docker_info.prom
 
-# Log last successful update - if "docker info" hangs this script will also hang and no update will be written
-echo "node_docker_last_successful_update `date +%s`" >/var/lib/node_exporter/textfile_collector/docker_last_update.prom
-
+mv /tmp/docker_info.prom /var/lib/node_exporter/textfile_collector

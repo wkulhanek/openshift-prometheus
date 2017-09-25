a1451c05e938498eb28451af304148490196d103
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-09-25 16:52:20 -0400

    Updated to Prometheus 2.0.0-beta5

--- prometheus-image/build.sh
+++ prometheus-image/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t wkulhanek/prometheus:latest
-docker tag wkulhanek/prometheus:latest wkulhanek/prometheus:2.0.0-beta4
+docker tag wkulhanek/prometheus:latest wkulhanek/prometheus:2.0.0-beta5
 docker push wkulhanek/prometheus:latest
-docker push wkulhanek/prometheus:2.0.0-beta4
+docker push wkulhanek/prometheus:2.0.0-beta5

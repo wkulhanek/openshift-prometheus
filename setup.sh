44c5b0c6dd78b061ba963f923a359b11136a8983
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-10-06 17:02:32 -0400

    Updated setup

--- setup.sh
+++ setup.sh
@@ -2,5 +2,7 @@
 oc project prometheus
 if [ "$?" != "0" ]; then
   oc new-project prometheus --display-name="Prometheus Monitoring"
+  oc annotate namespace prometheus openshift.io/node-selector=""
 fi
 oc new-app -f prometheus.yaml --param ROUTER_PASSWORD=$(oc set env dc router -n default --list|grep STATS_PASSWORD|awk -F"=" '{print $2}')
+oc adm policy add-scc-to-user privileged prometheus

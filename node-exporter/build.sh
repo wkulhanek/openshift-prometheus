fa06eb24fd60a64f6d72092274e77288a93b4484
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2017-12-08 10:33:20 -0500

    Updated to Node Exporter 0.15.2

--- node-exporter/build.sh
+++ node-exporter/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t wkulhanek/node-exporter:latest
-docker tag wkulhanek/node-exporter:latest wkulhanek/node-exporter:0.15.1
+docker tag wkulhanek/node-exporter:latest wkulhanek/node-exporter:0.15.2
 docker push wkulhanek/node-exporter:latest
-docker push wkulhanek/node-exporter:0.15.1
+docker push wkulhanek/node-exporter:0.15.2

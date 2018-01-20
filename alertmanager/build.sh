4bb3dee66d36b40142644bc1777e7f84b6968bc1
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-01-20 16:23:32 +0100

    Updated to Alertmanager 0.13.0

--- alertmanager/build.sh
+++ alertmanager/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t wkulhanek/alertmanager:latest
-docker tag wkulhanek/alertmanager:latest wkulhanek/alertmanager:0.12.0
+docker tag wkulhanek/alertmanager:latest wkulhanek/alertmanager:0.13.0
 docker push wkulhanek/alertmanager:latest
-docker push wkulhanek/alertmanager:0.12.0
+docker push wkulhanek/alertmanager:0.13.0

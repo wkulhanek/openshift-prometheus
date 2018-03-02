bf1e75e9fe6f394057d92d77198d57d361e62b89
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-03-02 10:30:58 +0100

    Revert "Updated to Prometheus 2.1.0"
    This reverts commit abc0ff24ba5c5d0b65eb58c33297a36c0ccb273a.

--- prometheus-image/build.sh
+++ prometheus-image/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build . -t wkulhanek/prometheus:latest
-docker tag wkulhanek/prometheus:latest wkulhanek/prometheus:2.1.0
+docker tag wkulhanek/prometheus:latest wkulhanek/prometheus:2.0.0-rc.2
 docker push wkulhanek/prometheus:latest
-docker push wkulhanek/prometheus:2.1.0
+docker push wkulhanek/prometheus:2.0.0-rc.2

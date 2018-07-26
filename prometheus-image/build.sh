bbbb663a7776dd928498da4f4c6e9923a25cb548
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2018-07-26 17:12:10 +0200

    Updated to Prometheus 2.3.2

--- prometheus-image/build.sh
+++ prometheus-image/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=2.3.1
+export VERSION=2.3.2
 docker build . -t wkulhanek/prometheus:latest
 docker tag wkulhanek/prometheus:latest wkulhanek/prometheus:${VERSION}
 docker push wkulhanek/prometheus:latest

331ac15a1f7359fd9828c56df3e80a58a4935075
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2019-03-18 08:40:31 -0400

    Removed Dockerfile.atomic, updated for Prometheus 2.8.0

--- prometheus-image/build.sh
+++ prometheus-image/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=2.7.1
+export VERSION=2.8.0
 docker build . -t wkulhanek/prometheus:latest
 docker tag wkulhanek/prometheus:latest wkulhanek/prometheus:${VERSION}
 docker push wkulhanek/prometheus:latest
